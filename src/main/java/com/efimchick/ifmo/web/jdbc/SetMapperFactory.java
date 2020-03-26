2ca9c8346d1aad00c6a48c67b74dab6298e61165
PinkDrifter <kalenik.igor@gmail.com>
2020-03-26 23:12:54 +0300

    setmapper is ready to pass travis

--- src/main/java/com/efimchick/ifmo/web/jdbc/SetMapperFactory.java
+++ src/main/java/com/efimchick/ifmo/web/jdbc/SetMapperFactory.java
@@ -1,12 +1,62 @@
 package com.efimchick.ifmo.web.jdbc;
 
-import java.util.Set;
+import java.math.BigInteger;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.*;
 
 import com.efimchick.ifmo.web.jdbc.domain.Employee;
+import com.efimchick.ifmo.web.jdbc.domain.FullName;
+import com.efimchick.ifmo.web.jdbc.domain.Position;
 
 public class SetMapperFactory {
 
     public SetMapper<Set<Employee>> employeesSetMapper() {
-        throw new UnsupportedOperationException();
+
+        return new SetMapper<Set<Employee>>() {
+            @Override
+            public Set<Employee> mapSet(ResultSet resultSet) {
+                Set<Employee> employees = new HashSet<>();
+                String sql = "select employee.* from employee";
+                try {
+                    while (resultSet.next()) employees.add(mapRowEmp(resultSet));
+                    return employees;
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                } return null;
+            }
+        };
+    }
+
+    private Employee mapRowEmp(ResultSet resultSet) throws SQLException {
+        Employee employee = new Employee(
+                BigInteger.valueOf(resultSet.getInt("id")),
+                new FullName(
+                        resultSet.getString("firstName"),
+                        resultSet.getString("lastName"),
+                        resultSet.getString("middleName")
+                ),
+                Position.valueOf(resultSet.getString("position")),
+                LocalDate.parse(resultSet.getString("hireDate")),
+                resultSet.getBigDecimal("salary"),
+                resultSet.getInt("manager")!=0 ? mapRowManager(resultSet) : null
+        );
+        return employee;
+    }
+
+    private Employee mapRowManager(ResultSet resultSet) throws SQLException {
+        int manId = resultSet.getInt("manager");
+        int curRow = resultSet.getRow();
+        Employee manager = null;
+        resultSet.beforeFirst();
+        while (resultSet.next()) {
+            if (resultSet.getInt("id") == manId) {
+                manager = mapRowEmp(resultSet);
+                break;
+            }
+        }
+        resultSet.absolute(curRow);
+        return manager;
     }
 }
