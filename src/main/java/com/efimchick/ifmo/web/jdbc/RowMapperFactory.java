9fe472c0c9c307a137e3b29cfdec3a6489576f12
PinkDrifter <kalenik.igor@gmail.com>
2020-03-26 22:34:00 +0300

    task-2 ready

--- src/main/java/com/efimchick/ifmo/web/jdbc/RowMapperFactory.java
+++ src/main/java/com/efimchick/ifmo/web/jdbc/RowMapperFactory.java
@@ -1,10 +1,33 @@
 package com.efimchick.ifmo.web.jdbc;
 
 import com.efimchick.ifmo.web.jdbc.domain.Employee;
+import com.efimchick.ifmo.web.jdbc.domain.FullName;
+import com.efimchick.ifmo.web.jdbc.domain.Position;
+
+import java.math.BigInteger;
+import java.sql.SQLException;
+import java.time.LocalDate;
 
 public class RowMapperFactory {
 
     public RowMapper<Employee> employeeRowMapper() {
-        throw new UnsupportedOperationException();
+            return resultSet -> {
+                try {
+                    return new Employee(
+                            BigInteger.valueOf(resultSet.getInt("id")),
+                            new FullName(
+                                    resultSet.getString("firstName"),
+                                    resultSet.getString("lastName"),
+                                    resultSet.getString("middleName")
+                            ),
+                            Position.valueOf(resultSet.getString("position")),
+                            LocalDate.parse(resultSet.getString("hireDate")),
+                            resultSet.getBigDecimal("salary")
+                    );
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                    return null;
+                }
+            };
     }
 }
